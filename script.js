457db9ce2cd11004d0d5f752a099f551da5045f7
Rayhan <rayhanprima99@gmail.com>
2022-03-12 13:32:52 +0700

    refactor : move function to outside

--- script.js
+++ script.js
@@ -8,29 +8,31 @@ let ctx = snakeCanvas.getContext("2d");
 ctx.canvas.width = CANVAR_SIZE;
 ctx.canvas.height = CANVAR_SIZE;
 
+const drawTitle = () => {
+    ctx.font = "24px Lalezar";
+    ctx.textAlign = "center";
+    ctx.fillText("START GAME", CANVAR_SIZE / 2, CANVAR_SIZE / 2);
+};
+
+const drawSubTitle = () => {
+    ctx.font = "18px Fredoka";
+    ctx.textAlign = "center";
+    ctx.fillText(
+        "-- Press any key to continue --",
+        CANVAR_SIZE / 2,
+        CANVAR_SIZE / 2 + 28
+    );
+};
+
+function drawLifes() {}
+
 function startGame() {
     ctx.clearRect(0, 0, CANVAR_SIZE, CANVAR_SIZE);
     ctx.fillStyle = SNAKE_COLOR;
-    ctx.fillRect(3 * CELL_SIZE, 5 * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+    ctx.fillRect(CELL_SIZE, CELL_SIZE, CELL_SIZE, CELL_SIZE);
 }
 
 function startMenu() {
-    const drawTitle = () => {
-        ctx.font = "24px Lalezar";
-        ctx.textAlign = "center";
-        ctx.fillText("START GAME", CANVAR_SIZE / 2, CANVAR_SIZE / 2);
-    };
-
-    const drawSubTitle = () => {
-        ctx.font = "18px Fredoka";
-        ctx.textAlign = "center";
-        ctx.fillText(
-            "-- Press any key to continue --",
-            CANVAR_SIZE / 2,
-            CANVAR_SIZE / 2 + 28
-        );
-    };
-
     drawTitle();
     drawSubTitle();
 
@@ -43,4 +45,4 @@ function startMenu() {
     );
 }
 
-startMenu();
+startGame();
