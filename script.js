96fc801b0ff808f7a6ea1e16dfce542a62ef5d7a
Rayhan <rayhanprima99@gmail.com>
2022-03-12 12:13:24 +0700

    chore: update press any key event

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
 //Press any key
 const CELL_SIZE = 30;
-const SNAKE_COLOR = 'orange';
+const SNAKE_COLOR = "orange";
+
+let snakeCanvas = document.getElementById("snakeBoard");
+let ctx = snakeCanvas.getContext("2d");
 
 function draw() {
-  alert('Click OK to start game');
-
-  let snakeCanvas = document.getElementById('snakeBoard');
-  let ctx = snakeCanvas.getContext('2d');
-
-  ctx.fillStyle = SNAKE_COLOR;
-  ctx.fillRect(3 * CELL_SIZE, 5 * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+    ctx.fillStyle = SNAKE_COLOR;
+    ctx.fillRect(3 * CELL_SIZE, 5 * CELL_SIZE, CELL_SIZE, CELL_SIZE);
 }
 
-var lastDownTarget, canvas;
-
 document.addEventListener(
-  'keydown',
-  function (event) {
-    if (lastDownTarget == canvas) {
-      alert('keydown');
-    }
-  },
-  false
+    "keydown",
+    () => {
+        if (lastDownTarget == canvas) {
+            alert("keydown");
+        }
+    },
+    false
 );
