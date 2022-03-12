a41f263af4f8fabf446d6bc74bd87388a51e89fc
Ahmad Farizi <ahmadfarizii17@gmail.com>
2022-03-12 12:00:53 +0700

    chore: add press any key button

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// do your magic here
+//Press any key
+const CELL_SIZE = 30;
+const SNAKE_COLOR = 'orange';
+
+function draw() {
+  alert('Click OK to start game');
+
+  let snakeCanvas = document.getElementById('snakeBoard');
+  let ctx = snakeCanvas.getContext('2d');
+
+  ctx.fillStyle = SNAKE_COLOR;
+  ctx.fillRect(3 * CELL_SIZE, 5 * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+}
+
+var lastDownTarget, canvas;
+
+document.addEventListener(
+  'keydown',
+  function (event) {
+    if (lastDownTarget == canvas) {
+      alert('keydown');
+    }
+  },
+  false
+);
