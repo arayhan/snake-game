b8ececc335ca56f9c2c3e06e69db7e442b04350d
Rayhan <rayhanprima99@gmail.com>
2022-03-12 12:40:56 +0700

    chore add start game state

--- script.js
+++ script.js
@@ -9,17 +9,39 @@ let ctx = snakeCanvas.getContext("2d");
 ctx.canvas.width = CANVAR_SIZE;
 ctx.canvas.height = CANVAR_SIZE;
 
-function draw() {
+function startGame() {
+    ctx.clearRect(0, 0, CANVAR_SIZE, CANVAR_SIZE);
     ctx.fillStyle = SNAKE_COLOR;
     ctx.fillRect(3 * CELL_SIZE, 5 * CELL_SIZE, CELL_SIZE, CELL_SIZE);
 }
 
-document.addEventListener(
-    "keydown",
-    () => {
-        if (lastDownTarget == canvas) {
-            alert("keydown");
-        }
-    },
-    false
-);
+function startMenu() {
+    const drawTitle = () => {
+        ctx.font = "24px Lalezar";
+        ctx.textAlign = "center";
+        ctx.fillText("START GAME", CANVAR_SIZE / 2, CANVAR_SIZE / 2);
+    };
+
+    const drawSubTitle = () => {
+        ctx.font = "18px Fredoka";
+        ctx.textAlign = "center";
+        ctx.fillText(
+            "-- Press any key to continue --",
+            CANVAR_SIZE / 2,
+            CANVAR_SIZE / 2 + 28
+        );
+    };
+
+    drawTitle();
+    drawSubTitle();
+
+    document.addEventListener(
+        "keydown",
+        () => {
+            startGame();
+        },
+        false
+    );
+}
+
+startMenu();
