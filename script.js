bf67f9f58c11f1d75c32be5b7da1d5ea03da7cfd
Rayhan <rayhanprima99@gmail.com>
2022-03-12 15:57:54 +0700

    chore: add new board for lifes & show remaining life

--- script.js
+++ script.js
@@ -1,58 +1,82 @@
-// ================================================
-// SECTION: INITIALIZATION
-// ================================================
+// ========================================== SECTION: INITIALIZATION
+let snakeCanvas = document.getElementById("snakeBoard");
+let lifeCanvas = document.getElementById("lifeBoard");
+
+let snakeCtx = snakeCanvas.getContext("2d");
+let lifeCtx = lifeCanvas.getContext("2d");
+
+const CANVAS_SIZE = snakeCtx.canvas.width;
 const CELL_SIZE = 20;
 const SNAKE_COLOR = "orange";
-const CANVAR_SIZE = 600;
+const REDRAW_INTERVAL = 20;
 
-let snakeCanvas = document.getElementById("snakeBoard");
-let ctx = snakeCanvas.getContext("2d");
-
-ctx.canvas.width = CANVAR_SIZE;
-ctx.canvas.height = CANVAR_SIZE;
-
-// ================================================
-// SECTION: GAMEPLAY
-// ================================================
-function drawSnake() {
-    ctx.fillStyle = SNAKE_COLOR;
-    ctx.fillRect(CELL_SIZE, CELL_SIZE, CELL_SIZE, CELL_SIZE);
+// ========================================== SECTION: GAMEPLAY
+function initPosition() {
+    return {
+        x: Math.floor(Math.random() * (CANVAS_SIZE - CELL_SIZE)),
+        x: Math.floor(Math.random() * (CANVAS_SIZE - CELL_SIZE)),
+        y: Math.floor(Math.random() * (CANVAS_SIZE - CELL_SIZE)),
+        y: Math.floor(Math.random() * (CANVAS_SIZE - CELL_SIZE)),
+    };
 }
 
-function drawLife() {
-    let img = new Image();
-    img.src = "./assets/images/heart-black.png";
+const LIFE = {
+    count: 3,
+    position: {
+        x: 10,
+        y: 10,
+    },
+};
 
-    ctx.drawImage(
-        img,
-        apple.position.x * CELL_SIZE,
-        apple.position.y * CELL_SIZE,
-        CELL_SIZE,
-        CELL_SIZE
-    );
+function clearCanvas() {
+    snakeCtx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+    snakeCtx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+    snakeCtx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+}
+
+function drawSnake() {
+    snakeCtx.fillStyle = SNAKE_COLOR;
+    snakeCtx.fillRect(CELL_SIZE, CELL_SIZE, CELL_SIZE, CELL_SIZE);
+}
+
+function drawLife(count, x, y) {
+    let img = new Image();
+    img.src = "/assets/images/heart-red.png";
+
+    for (let i = 0; i < count; i++) {
+        const margin = i * 3;
+        lifeCtx.drawImage(
+            img,
+            x + i * CELL_SIZE + margin,
+            y,
+            CELL_SIZE,
+            CELL_SIZE
+        );
+    }
 }
 
 function startGame() {
-    ctx.clearRect(0, 0, CANVAR_SIZE, CANVAR_SIZE);
-
-    drawSnake();
-    drawLife();
+    setInterval(function () {
+        clearCanvas();
+        drawSnake();
+        drawLife(LIFE.count, LIFE.position.x, LIFE.position.y);
+    }, REDRAW_INTERVAL);
 }
 
-// ================================================
-// SECTION: START MENU
-// ================================================
+// ========================================== SECTION: START MENU
 const drawTitle = () => {
-    ctx.font = "24px Lalezar";
-    ctx.textAlign = "center";
-    ctx.fillText("START GAME", CANVAR_SIZE / 2, CANVAR_SIZE / 2);
+    snakeCtx.font = "24px Lalezar";
+    snakeCtx.textAlign = "center";
+    snakeCtx.fillText("START GAME", CANVAS_SIZE / 2, CANVAS_SIZE / 2);
+    snakeCtx.fillText("START GAME", CANVAS_SIZE / 2, CANVAS_SIZE / 2);
+    snakeCtx.fillText("START GAME", CANVAS_SIZE / 2, CANVAS_SIZE / 2);
 
-    ctx.font = "18px Fredoka";
-    ctx.textAlign = "center";
-    ctx.fillText(
+    snakeCtx.font = "18px Fredoka";
+    snakeCtx.textAlign = "center";
+    snakeCtx.fillText(
         "-- Press any key to continue --",
-        CANVAR_SIZE / 2,
-        CANVAR_SIZE / 2 + 28
+        CANVAS_SIZE / 2,
+        CANVAS_SIZE / 2 + 30
     );
 };
 
@@ -64,7 +88,7 @@ function startMenu() {
     });
 }
 
-// ================================================
-// SECTION: INITIATION
-// ================================================
-startGame();
+// ========================================== SECTION: INITIATION
+function initialize() {
+    startGame();
+}
